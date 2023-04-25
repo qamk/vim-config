bbb355ddac9936557c7c0fba5831bf932ec5995b
queal <qamk@gmail.com>
2023-04-24 21:21:00 -0400

    add more colourschemes

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,6 +1,7 @@
 return {
     "bluz71/vim-nightfly-colors",
-    {"rebelot/kanagawa.nvim" , lazy= false, priority=1000, config = function () vim.cmd([[colorscheme kanagawa]]) end},
-    "folke/tokyonight.nvim",
+    "rebelot/kanagawa.nvim",
+    {"folke/tokyonight.nvim", lazy= false, priority=1000, config = function () vim.cmd([[colorscheme tokyonight]]) end},
     "jaredgorski/SpaceCamp",
+    "ellisonleao/gruvbox.nvim",
 }
