52dbf4b33a4abc1963e3ea02db2931f6fcf7f430
queal <qamking@gmail.com>
2024-08-05 17:59:24 -0400

    add and try out markview.nvim

--- lua/plugins/docs-and-markdown.lua
+++ lua/plugins/docs-and-markdown.lua
@@ -1,3 +1,4 @@
 return {
-    {"iamcco/markdown-preview.nvim", build = "cd app && npm install", init = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = "markdown"},
+    {"OXY2DEV/markview.nvim", lazy = false,  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }},
+    -- {"iamcco/markdown-preview.nvim" , build = "cd app && npm install" , init = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = "markdown"}
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,5 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
     "p00f/nvim-ts-rainbow",
     "nvim-treesitter/playground",
 }
