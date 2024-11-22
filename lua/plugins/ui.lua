ce3e9816bb9a6f3b7769018775bb4cdffb3c6d2b
queal <qamking@gmail.com>
2024-11-22 12:16:39 -0500

    minor corrections

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,6 @@
 return {
-    { "nvim-lualine/lualine.nvim"
-        , dependencies = { "nvim-tree/nvim-web-devicons" }
+    { "nvim-tree/nvim-web-devicons" }
+    , { "nvim-lualine/lualine.nvim"
         , opts = {
             theme = "auto"
     } }
